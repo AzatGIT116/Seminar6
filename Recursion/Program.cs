8f388b613bca5a270dff16ba5dbd5dc465f6bc4b
Azat Minimullin <minimullin.azat@yandex.ru>
2022-07-16 00:26:48 +0300

    add Recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿void SomeFunc(int a)
+{
+    Console.WriteLine($"На входе {a}");
+    if (a < 0)
+        return;
+
+    Console.WriteLine(a);
+    SomeFunc(a - 1);
+    Console.WriteLine($"На выходе {a}");
+}
+
+SomeFunc(5);
